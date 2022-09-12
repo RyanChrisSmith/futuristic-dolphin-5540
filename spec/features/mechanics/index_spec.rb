dd6b6ffcb80bd80ecdf5246cba12dad31dc09ae6
Ryan Smith <106180616+RyanChrisSmith@users.noreply.github.com>
2022-09-12 09:39:43 -0600

    set up for test on story 1

---
+++ spec/features/mechanics/index_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe 'Mechanics index page' do 
+  describe 'story 1' do
+    it 'displays All Mechanics at the top of the page'
+
+    it 'lists all the mechanics, their names, and years of experience'
+
+    it 'displays the average years of experience of all mechanics'
+  end
+  
+end
