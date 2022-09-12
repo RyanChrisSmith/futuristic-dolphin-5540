ba4638bb272b7f691af1c787c1c2a5bc1ac5483c
Ryan Smith <106180616+RyanChrisSmith@users.noreply.github.com>
2022-09-12 11:12:53 -0600

    last test for story 2 added

--- spec/features/mechanics/show_spec.rb
+++ spec/features/mechanics/show_spec.rb
@@ -58,7 +58,27 @@ RSpec.describe 'Mechanics show page' do
       expect(page).to_not have_content("Ferris Wheel")
     end
 
-    it 'And the rides are listed by thrill rating in descending order (most thrills first)'
+    it 'And the rides are listed by thrill rating in descending order (most thrills first)' do
+      ryan = Mechanic.create!(name: "Ryan", years_experience: 12)
+      six_flags = AmusementPark.create!(name: 'Six Flags', admission_cost: 75)
+      universal = AmusementPark.create!(name: 'Universal Studios', admission_cost: 80)
+      hurler = six_flags.rides.create!(name: 'The Hurler', thrill_rating: 7, open: true)
+      scrambler = six_flags.rides.create!(name: 'The Scrambler', thrill_rating: 4, open: true)
+      ferris = six_flags.rides.create!(name: 'Ferris Wheel', thrill_rating: 7, open: false)
+      jaws = universal.rides.create!(name: 'Jaws', thrill_rating: 5, open: true)
+      RideMechanic.create!(ride: hurler, mechanic: ryan)
+      RideMechanic.create!(ride: scrambler, mechanic: ryan)
+      RideMechanic.create!(ride: ferris, mechanic: ryan)
+      RideMechanic.create!(ride: jaws, mechanic: ryan)
+
+      visit "/mechanics/#{ryan.id}"
+
+      expect(ferris).to appear_before(hurler)
+      expect(hurler).to appear_before(jaws)
+      expect(jaws).to appear_before(scrambler)
+      expect(hurler).to_not appear_before(ferris)
+    end
+
 
   end
 end
